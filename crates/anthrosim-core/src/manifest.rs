91534fb3ef88ab39f04c84a0994217a6b68fdd98
Jack <jack.dean1@btinternet.com>
2026-08-22 00:45:26 +0100

    Add population provenance to run manifest

--- crates/anthrosim-core/src/manifest.rs
+++ crates/anthrosim-core/src/manifest.rs
@@ -1,6 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{config::ExperimentConfig, time::SimTime, world::WorldSummary};
+use crate::{
+    config::ExperimentConfig,
+    population::PopulationSummary,
+    time::SimTime,
+    world::WorldSummary,
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -17,11 +22,12 @@ pub struct RunManifest {
     pub git_commit: Option<String>,
     pub experiment: ExperimentConfig,
     pub world: WorldSummary,
+    pub population: PopulationSummary,
     pub start_time: SimTime,
     pub end_time: SimTime,
     pub stop_reason: StopReason,
 }
 
 impl RunManifest {
-    pub const CURRENT_SCHEMA_VERSION: u32 = 2;
+    pub const CURRENT_SCHEMA_VERSION: u32 = 3;
 }
